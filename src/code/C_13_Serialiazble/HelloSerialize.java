298d17464270629c8c216734f596a69acaaa25e8
waveaway77 <waveaway77@knou.ac.kr>
2025-03-21 14:38:04 +0900

    serialization

---
+++ src/code/C_13_Serialiazble/HelloSerialize.java
@@ -0,0 +1,8 @@
+package code.C_13_Serialiazble;
+
+import java.io.Serializable;
+
+// 출처 : https://velog.io/@juhyeon1114/Java-Serializable.-%EC%A7%81%EB%A0%AC%ED%99%94%EB%9E%80-%EB%AC%B4%EC%97%87%EC%9D%BC%EA%B9%8C
+public class HelloSerialize implements Serializable {
+    //
+}
---
+++ src/code/C_13_Serialiazble/TestController.java
@@ -0,0 +1,25 @@
+package code.C_13_Serialiazble;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
+public class TestController {
+    public static void main(String[] args) throws IOException {
+
+        HelloSerialize helloSerialize = new HelloSerialize();
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(helloSerialize);
+
+        byte [] serialized;
+        serialized = byteArrayOutputStream.toByteArray();
+
+        System.out.println("serialized = "+ Arrays.toString(serialized));
+        /*
+serialized = [-84, -19, 0, 5, 115, 114, 0, 37, 99, 111, 100, 101, 46, 67, 95, 49, 51, 95, 83, 101, 114, 105, 97, 108, 105, 97, 122, 98, 108, 101, 46, 72, 101, 108, 108, 111, 83, 101, 114, 105, 97, 108, 105, 122, 101, 3, 119, -100, -75, -47, -63, 38, -60, 2, 0, 0, 120, 112]
+         */
+    }
+}
