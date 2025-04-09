37643b6f41fd287a4f86e47e48862667a29df71f
waveaway77 <waveaway77@knou.ac.kr>
2025-04-09 13:54:48 +0900

    constructor

--- src/code/C_22_Constructor/Test.kt
+++ src/code/C_22_Constructor/Test.kt
@@ -1,7 +1,9 @@
 package code.C_22_Constructor
 
 class Test {
-    fun testMethod() {}
+    fun testMethod(): Test {
+        return this
+    }
 }
 
 class Test2(name:String, age:Int) {} // 기본생성자 추가
@@ -31,14 +33,20 @@ class Test3(name:String, age:Int) {
 // init 사용하지 않고 파라미터를 사용하는 코드
 // 기본생성자의 매개변수 옆에 var과 val가 슬쩍 생긴것을 볼 수 있다.
 // 얘네들을 붙혀주면 생성자의 매개변수를 클래스 내부에서 사용할 수 있다.
-class TestClass(var name:String, var age:Int) {
+class Test4(var name:String, var age:Int) {
     fun introduce() {
         println("Hi I'm ${name} and ${age} years old.")
     }
 }
 
-class TestRestController(private val serviceFactory: TestClass) { // var을 붙이면 생성자의 매개변수를
-    fun doSomething(): TestClass {
-        return serviceFactory // 클래스내부에서도 쓸 수 있다
+class TestRestController(private val serviceFactory: Test) { // var을 붙이면 생성자의 매개변수를
+    fun doSomething(): Test {
+        return serviceFactory.testMethod() // 클래스내부에서도 쓸 수 있다
     }
+}
+
+class TestRestController2(serviceFactory: Test) { // var을 안 붙이면 생성자의 매개변수를
+//    fun doSomething(): Test {
+//        return serviceFactory.testMethod() // error! // 클래스내부에서 쓸 수 없음
+//    }
 }
