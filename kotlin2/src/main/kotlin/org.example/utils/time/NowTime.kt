4b86215cf835641370e4b6c863d55648c0af1553
waveaway77 <waveaway77@knou.ac.kr>
2025-04-21 13:12:55 +0900

    일자관련

--- kotlin2/src/main/kotlin/org.example/utils/time/NowTime.kt
+++ kotlin2/src/main/kotlin/org.example/utils/time/NowTime.kt
@@ -3,9 +3,14 @@ package org.example.org.example.utils.time
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+// LocalDateTime
 val current = LocalDateTime.now()
+
+// Formatter
 val formatter1 = DateTimeFormatter.ISO_LOCAL_DATE // 2025-04-27
 val formatter2 = DateTimeFormatter.ISO_LOCAL_DATE_TIME // 2025-04-17T17:49:35.3189044
+
+// 양식만들기
 val formatter3 = DateTimeFormatter.ofPattern("yyyyMMddHHmmss") // 20250417175021
 val formatter4 = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초") // 2025년 04월 17일 17시 50분 48초
 val formatted = current.format(formatter4)
