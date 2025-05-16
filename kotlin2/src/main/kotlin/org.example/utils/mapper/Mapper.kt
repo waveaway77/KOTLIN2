05b706cbf67dd573f6909f54c00917c86318adc3
waveaway77 <waveaway77@knou.ac.kr>
2025-05-16 13:10:00 +0900

    object mapper

---
+++ kotlin2/src/main/kotlin/org.example/utils/mapper/Mapper.kt
@@ -0,0 +1,24 @@
+package org.example.org.example.utils.mapper
+
+import com.fasterxml.jackson.databind.ObjectMapper
+
+fun main() {
+
+    val response = Any::class.java
+    val fromResponse = FromResponse("hi")
+
+    val objectMapper = ObjectMapper()
+    val convertedResponse1 = objectMapper.convertValue(response, ToResponse::class.java)
+    val convertedResponse2 = objectMapper.convertValue(fromResponse, ToResponse::class.java)
+
+    println(convertedResponse1.dummyName)
+    println(convertedResponse2.dummyName)
+}
+
+data class FromResponse(
+    val dummyName: String
+)
+
+data class ToResponse(
+    val dummyName: String
+)
